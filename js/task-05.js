13e7a808d8fbb13b97195c244283d0c38fb3f39f
Sergii Kurdybakha <103501357+s-kurdybakha@users.noreply.github.com>
2022-08-22 22:53:16 +0300

    5 завдання

---
+++ js/task-05.js
@@ -0,0 +1,18 @@
+const nameInput = document.querySelector('#name-input');
+const nameOutput = document.querySelector('#name-output');
+
+nameInput.addEventListener('input', event => {
+    nameOutput.textContent = event.target.value;
+
+    if (event.target.value === '') {
+        nameOutput.textContent = 'Anonymous';
+    }
+});
+
+
+// Завдання 5
+// Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input), 
+// підставляє його поточне значення в span#name-output. Якщо інпут порожній, у спані повинен відображатися рядок "Anonymous".
+
+// <input type="text" id="name-input" placeholder="Please enter your name" />
+// <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
