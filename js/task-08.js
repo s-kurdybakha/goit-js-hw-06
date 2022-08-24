dd8848db70213bf6f012fea10d01e59027ef1060
Sergii Kurdybakha <103501357+s-kurdybakha@users.noreply.github.com>
2022-08-24 23:14:48 +0300

    done task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,37 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onSubmit)
+
+function onSubmit (event) {
+    event.preventDefault();
+
+    if (event.currentTarget.email.value === '' || event.currentTarget.password.value === '') {
+            alert('Прошу заповнити всі поля @(*_*)@');
+        }
+
+        else {console.log({ email: event.currentTarget.email.value, password: event.currentTarget.password.value});
+            event.target.reset();
+        }
+}
+
+// Напиши скрипт управління формою логіна.
+
+// <form class="login-form">
+//   <label>
+//     Email
+//     <input type="email" name="email" />
+//   </label>
+//   <label>
+//     Password
+//     <input type="password" name="password" />
+//   </label>
+//   <button type="submit">Login</button>
+// </form>
+
+// 1. Обробка відправлення форми form.login-form повинна відбуватися відповідно до події submit.
+// 2. Під час відправлення форми сторінка не повинна перезавантажуватися.
+// 3. Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені.
+// 4. Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, 
+// а значення поля - значенням властивості. 
+// Для доступу до елементів форми використовуй властивість elements.
+// 5. Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
